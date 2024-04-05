0571688bb81e8bd866baafd495fd8119806a42a8
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2024-04-05 13:59:02 +0200

    main.py add basic commands and implementations

--- src/main.py
+++ src/main.py
@@ -1,34 +1,88 @@
+import logging as log
 import os
 from pathlib import Path
+from sys import stdout
+from typing import Optional
 
+import ruamel.yaml
 import typer
+from typer import Typer
+from typing_extensions import Annotated
 
-import yaml_service
+from yaml_file_manager import YamlFileManager
 
-app = typer.Typer()
-current_working_directory = Path(os.getcwd())
+log.basicConfig(format='%(levelname)s:%(message)s', stream=stdout, level=log.DEBUG)
+
+app_name = "upnext"
+app: Typer = typer.Typer()
+
+current_working_directory: Path = Path(os.getcwd())
+yaml_file_manager: YamlFileManager = YamlFileManager()
+series: dict | None = yaml_file_manager.find_series_by_path(str(current_working_directory))
 
 
-@app.callback()
-def init():
-    print("loading yaml file...")
-    yaml_service.read_yaml_string()
-
-
-@app.command()
-def save():
-    print("saving yaml file...")
-    yaml_service.write_yaml_string()
+def _ensure_series_not_null() -> dict:
+    if series is None:
+        raise ValueError(f"No series found. Please run '{app_name} init' first.")
+    return series
 
 
 @app.command()
 def info():
-    series = yaml_service.get_series_by_path(str(current_working_directory))
-    if series is None:
-        print("No series found.")
-        return
+    log.debug("COMMAND: info")
+    series: dict = _ensure_series_not_null()
+    print("---")
+    ruamel.yaml.YAML().dump([series], stdout)
 
-    print(f"Next episode: {series.next_episode}")
+
+@app.command()
+def init():
+    log.debug("COMMAND: init")
+    global series
+    if series:
+        print("Current directory is already initialized.")
+        info()
+    else:
+        print("Initializing current directory.")
+        series = yaml_file_manager.create_series_by_path(str(current_working_directory))
+        yaml_file_manager.save()
+        info()
+
+
+@app.command("set")
+def set_next_episode(n: int):
+    log.debug(f"COMMAND: set {n}")
+    series: dict = _ensure_series_not_null()
+    # TODO: validate n - not too big?
+    info()
+    series["next_episode"] = n
+    yaml_file_manager.save()
+    info()
+
+
+@app.command("watch")
+def increment_episode_count(n: Annotated[Optional[int], typer.Argument()] = 1):
+    log.debug(f"COMMAND: watch {n}")
+    series = _ensure_series_not_null()
+    set_next_episode(series["next_episode"] + n)
+
+
+@app.command()
+def reset():
+    log.debug("COMMAND: reset")
+    _ensure_series_not_null()
+    yaml_file_manager.remove_series_by_path(str(current_working_directory))
+    yaml_file_manager.save()
+    global series
+    series = None
+
+
+@app.command()
+def play():
+    log.debug("COMMAND: play")
+    series = _ensure_series_not_null()
+    info()
+    raise NotImplementedError("Not yet implemented!")
 
 
 if __name__ == "__main__":
