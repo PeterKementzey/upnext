231020807127ecae5624ee5dbf4f81afec6a80f0
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2024-06-09 00:33:54 +0200

    reorganize tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,77 +1,24 @@
 use utils::*;
 
-#[cfg(test)]
-mod utils {
-    use std::fs;
-    use std::path::PathBuf;
-    use std::process::Command;
-
-    fn toml_file_path() -> PathBuf {
-        let mut path = dirs::home_dir().unwrap();
-        path.push(".upnext.toml");
-        path
-    }
-
-    fn read_file() -> String {
-        fs::read_to_string(toml_file_path()).unwrap()
-    }
-
-
-    pub(crate) fn cargo_mainfest_dir() -> String {
-        std::env::var("CARGO_MANIFEST_DIR").unwrap()
-    }
-
-    pub(super) fn delete_file() {
-        let path = toml_file_path();
-        if path.exists() {
-            fs::remove_file(path).unwrap();
-        }
-    }
-
-    pub(super) fn set_file(content: String) {
-        fs::write(toml_file_path(), content).unwrap();
-    }
-
-    pub(super) fn assert_file_is(content: String) {
-        let file_content = read_file();
-        assert_eq!(file_content, content);
-    }
-
-    pub(super) fn run_upnext(args: Vec<&str>, expected_output: Option<&str>) {
-        let mut path = PathBuf::from(cargo_mainfest_dir());
-        path.push("target/debug/upnext");
-        println!("path: {:?}", path);
-        let output = Command::new(path)
-            .args(args)
-            .output()
-            .expect("Failed to execute command");
-
-        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-
-        if let Some(expected_output) = expected_output {
-            assert_eq!(&output.stdout[..], expected_output.as_bytes());
-        }
-    }
-}
-
 #[test]
 fn test_init() {
-    delete_file();
-    run_upnext(vec!["init"], Some("Initializing series\n"));
-    assert_file_is(format!(
+    delete_toml_file();
+    run_app(vec!["init"], Some(&format!(
+        "path = \"{}\"
+next_episode = 1\n\n", cargo_mainfest_dir())), Some(""));
+    assert_toml_file_is(format!(
         "[[series]]
 path = \"{}\"
 next_episode = 1
 ", cargo_mainfest_dir()));
 }
 
-
 #[test]
 fn test_init_twice() {
-    delete_file();
-    run_upnext(vec!["init"], Some("Initializing series\n"));
-    run_upnext(vec!["init"], None);
-    assert_file_is(format!(
+    delete_toml_file();
+    test_init();
+    run_app(vec!["init"], Some(""), Some("Current directory is already initialized.\n"));
+    assert_toml_file_is(format!(
         "[[series]]
 path = \"{}\"
 next_episode = 1
@@ -80,7 +27,8 @@ next_episode = 1
 
 #[test]
 fn test_increment_episode() {
-    set_file(format!(
+    delete_toml_file();
+    set_toml_file(format!(
         "# 1
 [[series]] # 2
 # 3
@@ -91,10 +39,9 @@ fn test_increment_episode() {
        next_episode = 1 # 8
        # 9
        ", cargo_mainfest_dir()));
-    run_upnext(vec!["inc", "2"], Some("Incrementing episode by 2\n"));
-    assert_file_is(
-        format!(
-            "# 1
+    run_app(vec!["inc", "2"], Some("Incrementing episode by 2\n"), None);
+    assert_toml_file_is(format!(
+        "# 1
 [[series]] # 2
 # 3
      # 4
@@ -104,5 +51,63 @@ fn test_increment_episode() {
        next_episode = 3 # 8
        # 9
        ",
-            cargo_mainfest_dir()));
+        cargo_mainfest_dir()));
+}
+
+
+#[cfg(test)]
+mod utils {
+    use std::fs;
+    use std::path::PathBuf;
+    use std::process::Command;
+
+    pub(super) fn cargo_mainfest_dir() -> String {
+        std::env::var("CARGO_MANIFEST_DIR").unwrap()
+    }
+
+    pub(super) fn delete_toml_file() {
+        let path = toml_file_path();
+        if path.exists() {
+            fs::remove_file(path).unwrap();
+        }
+    }
+
+    pub(super) fn set_toml_file(content: String) {
+        fs::write(toml_file_path(), content).unwrap();
+    }
+
+    pub(super) fn assert_toml_file_is(content: String) {
+        let file_content = read_file();
+        assert_eq!(file_content, content);
+    }
+
+    pub(super) fn run_app(args: Vec<&str>, expected_stdout: Option<&str>, expected_stderr: Option<&str>) {
+        let mut path = PathBuf::from(cargo_mainfest_dir());
+        path.push("target/debug/upnext");
+        println!("path: {:?}", path);
+        let output = Command::new(path)
+            .args(args)
+            .output()
+            .expect("Failed to execute command");
+
+        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+        println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+        if let Some(expected_output) = expected_stdout {
+            assert_eq!(&output.stdout[..], expected_output.as_bytes());
+        }
+        if let Some(expected_output) = expected_stderr {
+            assert_eq!(&output.stderr[..], expected_output.as_bytes());
+        }
+    }
+
+    fn toml_file_path() -> PathBuf {
+        let mut path = dirs::home_dir().unwrap();
+        path.push(".upnext.toml");
+        path
+    }
+
+    fn read_file() -> String {
+        fs::read_to_string(toml_file_path()).unwrap()
+    }
 }
