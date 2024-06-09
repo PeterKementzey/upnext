6ebb4bb040ec5ef6337fca8f7b8a9adac5c0d84f
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2024-06-09 02:27:13 +0200

    Refactor tests, use external files and added mutex for concurrent run

--- src/tests.rs
+++ src/tests.rs
@@ -1,57 +1,18 @@
-use utils::*;
+use crate::tests::utils::test;
 
 #[test]
 fn test_init() {
-    delete_toml_file();
-    run_app(vec!["init"], Some(&format!(
-        "path = \"{}\"
-next_episode = 1\n\n", cargo_mainfest_dir())), Some(""));
-    assert_toml_file_is(format!(
-        "[[series]]
-path = \"{}\"
-next_episode = 1
-", cargo_mainfest_dir()));
+    test("test_init", vec!["init"]);
 }
 
 #[test]
 fn test_init_twice() {
-    delete_toml_file();
-    test_init();
-    run_app(vec!["init"], Some(""), Some("Current directory is already initialized.\n"));
-    assert_toml_file_is(format!(
-        "[[series]]
-path = \"{}\"
-next_episode = 1
-", cargo_mainfest_dir()));
+    test("test_init_twice", vec!["init"]);
 }
 
 #[test]
 fn test_increment_episode() {
-    delete_toml_file();
-    set_toml_file(format!(
-        "# 1
-[[series]] # 2
-# 3
-     # 4
-       #5
-       path = \"{}\" # 6666
-       # 7
-       next_episode = 1 # 8
-       # 9
-       ", cargo_mainfest_dir()));
-    run_app(vec!["inc", "2"], Some("Incrementing episode by 2\n"), None);
-    assert_toml_file_is(format!(
-        "# 1
-[[series]] # 2
-# 3
-     # 4
-       #5
-       path = \"{}\" # 6666
-       # 7
-       next_episode = 3 # 8
-       # 9
-       ",
-        cargo_mainfest_dir()));
+    test("test_increment_episode", vec!["inc", "2"]);
 }
 
 
@@ -61,44 +22,34 @@ mod utils {
     use std::path::PathBuf;
     use std::process::Command;
 
-    pub(super) fn cargo_mainfest_dir() -> String {
+    static MUTEX: std::sync::Mutex<i32> = std::sync::Mutex::new(1);
+
+    pub fn test(name: &str, args: Vec<&str>) {
+        let (expected_stdout, expected_stderr, before, after) = read_test_files(name);
+        let (stdout, stderr, file_content) = match MUTEX.lock() {
+            Ok(_guard) => {
+                if let Some(before) = before {
+                    set_toml_file(before);
+                } else {
+                    delete_toml_file();
+                }
+                run_app(args)
+            }
+            Err(poisoned) => panic!("Mutex poisoned: {:?}", poisoned),
+        };
+        println!("stdout: {}", String::from_utf8_lossy(&stdout));
+        println!("stderr: {}", String::from_utf8_lossy(&stderr));
+        if let Some(expected_stdout) = expected_stdout {
+            assert_eq!(String::from_utf8_lossy(&stdout), expected_stdout);
+        }
+        if let Some(expected_stderr) = expected_stderr {
+            assert_eq!(String::from_utf8_lossy(&stderr), expected_stderr);
+        }
+        assert_eq!(file_content, after);
+    }
+
+    fn cargo_mainfest_dir() -> String {
         std::env::var("CARGO_MANIFEST_DIR").unwrap()
-    }
-
-    pub(super) fn delete_toml_file() {
-        let path = toml_file_path();
-        if path.exists() {
-            fs::remove_file(path).unwrap();
-        }
-    }
-
-    pub(super) fn set_toml_file(content: String) {
-        fs::write(toml_file_path(), content).unwrap();
-    }
-
-    pub(super) fn assert_toml_file_is(content: String) {
-        let file_content = read_file();
-        assert_eq!(file_content, content);
-    }
-
-    pub(super) fn run_app(args: Vec<&str>, expected_stdout: Option<&str>, expected_stderr: Option<&str>) {
-        let mut path = PathBuf::from(cargo_mainfest_dir());
-        path.push("target/debug/upnext");
-        println!("path: {:?}", path);
-        let output = Command::new(path)
-            .args(args)
-            .output()
-            .expect("Failed to execute command");
-
-        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-        println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-
-        if let Some(expected_output) = expected_stdout {
-            assert_eq!(&output.stdout[..], expected_output.as_bytes());
-        }
-        if let Some(expected_output) = expected_stderr {
-            assert_eq!(&output.stderr[..], expected_output.as_bytes());
-        }
     }
 
     fn toml_file_path() -> PathBuf {
@@ -107,7 +58,59 @@ mod utils {
         path
     }
 
-    fn read_file() -> String {
-        fs::read_to_string(toml_file_path()).unwrap()
+    fn delete_toml_file() {
+        let path = toml_file_path();
+        if path.exists() {
+            fs::remove_file(path).unwrap();
+        }
+    }
+
+    fn set_toml_file(content: String) {
+        fs::write(toml_file_path(), content).unwrap();
+    }
+
+    fn run_app(args: Vec<&str>) -> (Vec<u8>, Vec<u8>, Option<String>) {
+        let mut path = PathBuf::from(cargo_mainfest_dir());
+        path.push("target/debug/upnext");
+        let output = Command::new(path)
+            .args(args)
+            .output()
+            .expect("Failed to execute command");
+        let file_content = fs::read_to_string(&toml_file_path()).unwrap();
+
+        (output.stdout, output.stderr, Some(file_content))
+    }
+
+    /// returns expected_stdout, expected_stderr, .upnext.toml at start, .upnext.toml at end
+    fn read_test_files(test_name: &str) -> (Option<String>, Option<String>, Option<String>, Option<String>) {
+        fn inject_path(content: String) -> String {
+            content.replace("PATH", &cargo_mainfest_dir())
+        }
+
+        let path = {
+            let mut path = PathBuf::from(cargo_mainfest_dir());
+            path.push("test-resources");
+            path.push(test_name);
+            path
+        };
+
+        let res: Vec<Option<String>> = (0..4).map(|i| {
+            let mut path = path.clone();
+            path.push(match i {
+                0 => "stdout.txt",
+                1 => "stderr.txt",
+                2 => "before.toml",
+                3 => "after.toml",
+                _ => unreachable!(),
+            });
+            println!("-----path: {:?}", path);
+            fs::read_to_string(&path).map(inject_path).ok()
+        }).collect();
+
+        assert!(res.iter().any(|x| x.is_some()));
+
+        let res = (res[0].clone(), res[1].clone(), res[2].clone(), res[3].clone());
+        println!("------res: {:?}", res);
+        res
     }
 }
